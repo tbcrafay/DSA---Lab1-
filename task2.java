23e3c4cb928b1eae9a4058270983f25478dcf681
Abdul Rafay <140069786+tbcrafay@users.noreply.github.com>
2024-10-16 15:50:06 +0500

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        double d = 3.14;
+        Double wrapper = Double.valueOf(d);
+        System.out.println(wrapper);
+    }
+}
