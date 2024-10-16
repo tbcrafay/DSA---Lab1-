e42f7f5a0302c81a895986dc05770448c89a0c19
Abdul Rafay <140069786+tbcrafay@users.noreply.github.com>
2024-10-16 15:46:34 +0500

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,24 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String s1 = "How are you!?";
+        String str = "I'm good";
+        String str2 = new String("Java");
+        //intern method:
+        String s5 = str2.intern();
+        System.out.println(s1);
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(s5);
+    }
+}
+
+/*
+Output :
+java -cp /tmp/yeBrahzk1o/HelloWorld
+How are you!?
+I'm good
+Java
+Java
+
+=== Code Execution Successful ===
+*/
