7d0d585997d782ff17b77b021ad290757a916bcd
Abdul Rafay <140069786+tbcrafay@users.noreply.github.com>
2024-10-16 16:08:22 +0500

    Update task1.java

--- task1.java
+++ task1.java
@@ -2,12 +2,12 @@ class HelloWorld {
     public static void main(String[] args) {
         String s1 = "How are you!?";
         String str = "I'm good";
-        String str = str.concat(":)");
+        String str1 = str.concat(":)");
         String str2 = new String("Java");
         //intern method:
         String s5 = str2.intern();
         System.out.println(s1);
-        System.out.println(str);
+        System.out.println(str1);
         System.out.println(str2);
         System.out.println(s5);
     }
